7502634163645ca7e70c283e1f04d361d55c271a
Taylor Hight <Taylorthight@gmail.com>
2023-04-11 15:29:47 -0800

    update

--- app.py
+++ app.py
@@ -14,17 +14,19 @@ class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
   completed = db.Column(db.Boolean, nullable=False, default=False)
-  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=True)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
   def __repr__(self):
-    return f'<Todo {self.id} {self.description}>'
+    return f'<Todo {self.id} {self.description}, list {self.list_id}>'
   
 class TodoList(db.Model):
    __tablename__='todolists'
    id = db.Column(db.Integer, primary_key=True)
    name = db.Column(db.String(), nullable=False)
    todos = db.relationship('Todo', backref='list', lazy=True)
-                      
+
+def __repr__(self):
+   return f'<TodoList {self.id} {self.name}>'                     
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():   
@@ -36,13 +38,16 @@ def create_todo():
        body['description'] = todo.description
        db.session.add(todo)
        db.session.commit()
+       body['id'] = todo.id
+       body['completed'] = todo.completed
+       body['description'] = todo.description
    except:        
         error = True
         db.session.rollback()
         print(sys.exc_info())
    finally:
         db.session.close()           
-        if  error == True:
+        if  error:
             abort(400)
         else:            
             return jsonify(body)
@@ -72,9 +77,13 @@ def delete_todo(todo_id):
     db.session.close()
   return jsonify({ 'success': True })
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+   return redirect(url_for('get_list_todos', list_id=1))   
 
 if __name__ == '__main__':
     app.run(debug=True)
