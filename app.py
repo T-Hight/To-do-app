9d53e57b0885e527e1e6677c957d313845d8b623
Taylor Hight <Taylorthight@gmail.com>
2023-04-11 08:04:02 -0800

    lists_id

--- app.py
+++ app.py
@@ -2,23 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, a
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
 import sys
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:taymay12@localhost:5432/todoapp'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
   completed = db.Column(db.Boolean, nullable=False, default=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=True)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
-
-with app.app_context():
-    db.create_all()
-
+  
+class TodoList(db.Model):
+   __tablename__='todolists'
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(), nullable=False)
+   todos = db.relationship('Todo', backref='list', lazy=True)
+                      
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():   
